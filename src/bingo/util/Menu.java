f0609790459261c60bfe01eb15b5e83be7c06c8a
ayato <ayato.211210@gmail.com>
2026-09-08 14:45:01 +0900

    メインメニューのデザイン変更

--- src/bingo/util/Menu.java
+++ src/bingo/util/Menu.java
@@ -19,25 +19,25 @@ public class Menu {
 		printTitle("MAIN MENU");
 		//		System.out.println("PLAYER NAME	: " + Color.ORANGE + player.getName() + Color.RESET);
 		System.out.println("┌─ " + Color.ORANGE + "PLAYER" + Color.RESET + " ─────────────────────────────┐");
-		System.out.println(
-				"│ NAME\t: " + Color.ORANGE + player.getName() + Color.RESET
-						+ "\t\t MODE\t: " + Color.ORANGE + player.getMode() + Color.RESET + "       │");
+		System.out.println("│ NAME : " + Color.ORANGE + player.getName() + Color.RESET);
+		System.out.println("│ MODE : " + Color.ORANGE + player.getMode() + Color.RESET);
 		System.out.println("└──────────────────────────────────────┘");
-
 		System.out.println("┌─ " + Color.ORANGE + "MODE" + Color.RESET + " ───────────────────────────────┐");
 
 		System.out.println("│ " + Color.ORANGE + "NORMAL" + Color.RESET + "     │ 3 × 3 BEST : "
-				+ Color.ORANGE + player.getBestTurn3x3() + Color.RESET + "          │");
+				+ Color.ORANGE + player.getBestTurn3x3() + Color.RESET);
+
 		System.out.println("│            │ 5 × 5 BEST : "
-				+ Color.ORANGE + player.getBestTurn5x5() + Color.RESET + "          │");
+				+ Color.ORANGE + player.getBestTurn5x5() + Color.RESET);
+
 		System.out.println("│            │ 7 × 7 BEST : "
-				+ Color.ORANGE + player.getBestTurn7x7() + Color.RESET + "          │");
+				+ Color.ORANGE + player.getBestTurn7x7() + Color.RESET);
 
 		System.out.println("│ " + Color.ORANGE + "ENDLESS" + Color.RESET + "    │ TOTAL TURN : "
-				+ Color.ORANGE + player.getEndlessTurn() + Color.RESET + "          │");
+				+ Color.ORANGE + player.getEndlessTurn() + Color.RESET);
 
-		System.out.println("│ " + Color.ORANGE + "CHALLENGE" + Color.RESET + "  │ CLEARED	  : "
-				+ Color.ORANGE + "Yes/No" + Color.RESET + "     │");
+		System.out.println("│ " + Color.ORANGE + "CHALLENGE" + Color.RESET + "  │ CLEARED 	  : "
+				+ Color.ORANGE + "Yes/No" + Color.RESET);
 
 		System.out.println("└──────────────────────────────────────┘");
 
